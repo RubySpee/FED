f438aab71b794eba8cae7de1cf8bec6fbf183e98
RubySpee <94399952+RubySpee@users.noreply.github.com>
2021-12-07 20:12:44 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var button = document.querySelector("h2");
+
+function komTevoorschijn(){
+    let hetMenu = document.querySelector("nav");
+    hetMenu.classList.toggle("toonNav");
+}
+
+button.addEventListener("click", komTevoorschijn);
