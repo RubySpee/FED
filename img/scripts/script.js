0be08b19ae722c906461ae5d4fef00ccd514d7af
RubySpee <94399952+RubySpee@users.noreply.github.com>
2021-12-15 14:25:06 +0100

    Add files via upload

---
+++ img/scripts/script.js
@@ -0,0 +1,24 @@
+//De code van het manu
+//Deze code komt van de opdracht over een uitklapbaar menu
+var button = document.querySelector("h2");
+
+function komTevoorschijn(){
+    let hetMenu = document.querySelector("nav");
+    hetMenu.classList.toggle("toonNav");
+}
+
+button.addEventListener("click", komTevoorschijn);
+
+
+
+//De code voor de darkmode
+//Bron: https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp (heb alleen het javascript van deze bron, de rest wat hoort bij darkmode
+//komt van het filmpje dat als bron staat bij de css pagina)
+var button = document.querySelector(".darkmode")
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+button.addEventListener("click", myFunction)
